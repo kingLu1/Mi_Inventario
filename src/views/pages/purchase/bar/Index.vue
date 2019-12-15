d679818f4eac9731b12df7132aaa916556106432
layefa <kingm4159@gmail.com>
2019-12-15 12:28:53 +0100

    purchasing/bar 'select quantity' - done

--- src/views/pages/purchase/bar/Index.vue
+++ src/views/pages/purchase/bar/Index.vue
@@ -14,34 +14,7 @@
         </tab-content>
 
         <!-- tab 3 content -->
-        <tab-content title="Select Quantity" class="mb-5" icon="feather icon-image">
-<!--          <div class="vx-row">-->
-<!--            <div class="vx-col md:w-1/2 w-full">-->
-<!--              <vs-input label="Event Name" v-model="eventName" class="w-full mt-5"/>-->
-<!--            </div>-->
-<!--            <div class="vx-col md:w-1/2 w-full">-->
-<!--              <vs-select v-model="city" class="w-full select-large mt-5" label="Event Location">-->
-<!--                <vs-select-item :key="index" :value="item.value" :text="item.text" v-for="(item,index) in cityOptions"-->
-<!--                                class="w-full"/>-->
-<!--              </vs-select>-->
-<!--            </div>-->
-<!--            <div class="vx-col md:w-1/2 w-full mt-5">-->
-<!--              <vs-select v-model="status" class="w-full select-large" label="Event Status">-->
-<!--                <vs-select-item :key="index" :value="item.value" :text="item.text" v-for="(item,index) in statusOptions"-->
-<!--                                class="w-full"/>-->
-<!--              </vs-select>-->
-<!--            </div>-->
-<!--            <div class="vx-col md:w-1/2 w-full md:mt-8">-->
-<!--              <div class="demo-alignment">-->
-<!--                <span>Requirements:</span>-->
-<!--                <div class="flex">-->
-<!--                  <vs-checkbox>Staffing</vs-checkbox>-->
-<!--                  <vs-checkbox>Catering</vs-checkbox>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </div>-->
-        </tab-content>
+
         <tab-content title="Check" class="mb-5" icon="feather icon-image">
 <!--          <div class="vx-row">-->
 <!--            <div class="vx-col md:w-1/2 w-full">-->
--- src/views/pages/purchase/bar/SelectQuantity.vue
+++ src/views/pages/purchase/bar/SelectQuantity.vue
@@ -1,14 +1,47 @@
 <template>
-  <div class="vx-row">
-    <div class="vx-row flex justify-center">
-      <div class="vx-col w-full sm:w-full md:w-1/2 lg:w-1/3  xl:w-1/3 mt-5">
-        <div class="vx-row flex">
-          <div class="centerx">
-            <!--            <vs-input-number v-model="" label="passengers:"/>-->
-          </div>
-        </div>
+  <div>
+    <div class="vx-row">
+      <div class="vx-col  w-1/5">
+        <p class="font-bold ">Name</p>
+      </div>
+      <div class="vx-col  w-1/5">
+        <p class="font-bold text-center">Crate Price</p>
+      </div>
+      <div class="vx-col  w-1/5">
+        <p class="font-bold text-center">Quantity Per Crate</p>
+      </div>
+
+      <div class="vx-col  w-1/5">
+        <p class="font-bold text-center">Purchasing</p>
+      </div>
+      <div class="vx-col  w-1/5">
+        <p class="font-bold text-center">Amount</p>
       </div>
     </div>
+    <div class="vx-row flex items-center" v-for="p in purchase">
+
+      <div class="vx-col   w-1/5">
+        <p>{{p.name | capitalize}}</p>
+      </div>
+
+      <div class="vx-col flex justify-center w-1/5">
+        <p>{{p.crate_price | currency}}</p>
+      </div>
+      <div class="vx-col flex justify-center w-1/5">
+        <p>{{p.qty_per_crate}}</p>
+      </div>
+      <div class="vx-col  w-1/5">
+        <p class="flex">
+          <span class="centerx">
+            <vs-input-number v-model="p.purchasing" :label="totalQuantity(p)"/>
+          </span>
+        </p>
+      </div>
+      <div class="vx-col  flex justify-center w-1/5">
+        <p class="money">{{p.crate_price * p.purchasing | currency}}</p>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -20,6 +53,33 @@
         required: true
       }
     },
+    data: () => ({
+      passengers: 1,
+      purchase: []
+    }),
+    methods: {
+      trimChipsValue() {
+        this.purchase = this.chips.map(item => {
+            return {
+              id: item.id,
+              name: item.name,
+              vendor: item.vendor,
+              qty_per_crate: item.qty_per_crate,
+              crate_price: item.crate_price,
+              purchasing: 0
+            }
+          }
+        )
+      },
+      totalQuantity(p) {
+        return `Crate:(${p.qty_per_crate * p.purchasing})`
+      }
+    },
+    watch: {
+      chips() {
+        this.trimChipsValue()
+      }
+    }
   }
 </script>
 
